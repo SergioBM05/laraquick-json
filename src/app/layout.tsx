5094844e14fbba6bce2c5a248f2a6ca0c405a20e
SergioBM05 <serburmej@alu.edu.gva.es>
2026-03-23 15:04:00 +0100

    Verificacion puesta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: 'LaraQuick Tool - JSON to Laravel Converter',
   description: 'Convert your JSON schemas into Laravel Eloquent models in seconds.',
+  verification: {
+    google: 'WN6TqtQCdVLQE6j4qWpyOmx5mSxuZ1p5Ytw3QtYpZZM',
+  },
   openGraph: {
     title: 'LaraQuick Tool',
     description: 'The ultimate tool for Laravel developers',
